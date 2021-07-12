0b9daac552f6652591151c1f0f33a93032a0069c
mirel.voicu <mirel.voicu02@gmail.com>
2021-07-12 23:34:55 +0300

    some progress

--- prodhunt/settings.py
+++ prodhunt/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'prodhunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['prodhunt/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- prodhunt/urls.py
+++ prodhunt/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import products.views as products_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',products_views.home,name='home')
 ]
